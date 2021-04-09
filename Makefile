b59678fe785982c1bac7a6f7b5e24f37e7ff895f
abstrack <lebel.louisjacob@gmail.com>
2021-04-09 00:42:32 -0400

    application -> library

--- Makefile
+++
@@ -1,4 +0,0 @@
-# target sample rate
-SAMPLE_RATE := 44100
-
-include rules.mk
---
+++ include/musicpp.hpp
@@ -0,0 +1,23 @@
+#ifndef MPP_HPP
+#define MPP_HPP
+
+#include "generators/mix.hpp"
+#include "generators/size.hpp"
+#include "generators/offset.hpp"
+#include "generators/low_pass.hpp"
+#include "generators/high_pass.hpp"
+#include "generators/basic.hpp"
+
+#include "mixers/sample.hpp"
+
+#include "controls/bezier.hpp"
+#include "controls/steady.hpp"
+
+#include "generator.hpp"
+#include "mixer.hpp"
+#include "export.hpp"
+#include "config.hpp"
+
+#include "literals.hpp"
+
+#endif
